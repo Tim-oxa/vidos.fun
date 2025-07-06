7079ccc6e460279eff451f67b8721d3419795005
Tim <timsadnessboy@gmail.com>
2025-07-06 15:08:44 +0300

    Test

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+from quart import Quart, render_template
 
+
+app = Quart(__name__)
+
+
+@app.get("/")
+async def index():
+    video_src = "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WeAreGoingOnBullrun.mp4"
+    return await render_template("index.html", video_src=video_src)
+
+
+app.run("0.0.0.0", 116)
