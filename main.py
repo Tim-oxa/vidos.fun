46d4510aeb7e510586d1d79007db5bab6588de21
Tim <timsadnessboy@gmail.com>
2025-07-07 16:15:44 +0300

    Test

--- main.py
+++ main.py
@@ -1,50 +1,78 @@
-from quart import Quart, render_template, websocket
+from quart import Quart, render_template, websocket, redirect
+import asyncio
+import uuid
 
 
 app = Quart(__name__)
 
 
-spectators = set()
+rooms = {}
+
+
+async def broadcast(ws, room: str, data: dict):
+    try:
+        await ws.send_json(data)
+    except:
+        rooms[room]["spectators"].discard(ws)
 
 
 @app.get("/")
 async def index():
-    video_src = "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WeAreGoingOnBullrun.mp4"
-    return await render_template("index.html", video_src=video_src)
+    room = str(uuid.uuid4())
+    rooms.setdefault(room, {
+        "video": "https://vkvd551.okcdn.ru/?srcIp=77.37.182.155&pr=40&expires=1752348773320&srcAg=CHROME_MAC&fromCache=1&ms=185.226.55.189&type=5&subId=5282777991697&sig=p5x7YSVIsl4&ct=0&urls=45.136.20.178&clientType=13&appId=512000384397&id=8151257057809",
+        "spectators": set()
+    })
+    return redirect("/host/"+room)
 
 
-@app.get("/view")
-async def spectator_page():
-    video_src = "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WeAreGoingOnBullrun.mp4"
-    return await render_template("user.html", video_src=video_src)
+@app.get("/host/<room>")
+async def host_page(room: str):
+    if room in rooms:
+        return await render_template("index.html", room=room, video_src=rooms[room]["video"])
+    else:
+        return 404
 
 
-@app.websocket('/ws/host')
-async def host_ws():
-    try:
-        while True:
-            data = await websocket.receive_json()
-            print(data)
-            for ws in spectators.copy():
-                try:
-                    await ws.send_json(data)
-                except:
-                    spectators.discard(ws)
-    except:
-        pass
+@app.get("/<room>")
+async def spectator_page(room: str):
+    if room in rooms:
+        return await render_template("user.html", room=room, video_src=rooms[room]["video"])
+    else:
+        return 404
 
 
-@app.websocket('/ws/spectator')
-async def spectator_ws():
+@app.websocket("/ws/host/<room>")
+async def host_ws(room: str):
+    if room in rooms:
+        try:
+            while True:
+                data = await websocket.receive_json()
+                print(data)
+                tasks = []
+                for ws in rooms[room]["spectators"].copy():
+                    tasks.append(broadcast(ws, room, data))
+                await asyncio.gather(*tasks, return_exceptions=True)
+        except:
+            pass
+    else:
+        await websocket.close(1000, "Комната не найдена")
+
+
+@app.websocket("/ws/spectator/<room>")
+async def spectator_ws(room: str):
     ws = websocket._get_current_object()
-    spectators.add(ws)
-    try:
-        while True:
-            await websocket.receive()  # опционально
-    except:
-        pass
-    finally:
-        spectators.discard(ws)
+    if room in rooms:
+        rooms[room]["spectators"].add(ws)
+        try:
+            while True:
+                await websocket.receive()  # опционально
+        except:
+            pass
+        finally:
+            rooms[room]["spectators"].discard(ws)
+    else:
+        await websocket.close(1000, "Комната не найдена")
 
 
 app.run("0.0.0.0", 116, debug=True)
